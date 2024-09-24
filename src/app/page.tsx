37acae37bbcc3e138a6cd73e59bc7ece0bac96a3
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-09-24 18:58:36 +0200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from './components/active-label';
+import NotActiveLabel from './components/not-active-label';
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not active</NotActiveLabel>
     </main>
   );
 }
